02bc83c6acc18b5088e64f42458a8ad89fd3bc8f
iskyd <iskyd@proton.me>
2024-03-30 12:56:23 +0100

    Added validation of grid

--- sudoku.hs
+++ sudoku.hs
@@ -1,4 +1,4 @@
-import Data.List (transpose)
+import Data.List (transpose, nub)
 
 type Value = Char
 type Row a = [a]
@@ -12,10 +12,19 @@ cols :: Matrix a -> [Row a]
 cols = transpose
 
 boxes :: Matrix a -> [Row a]
-boxes = map concat [(map (take 3 . drop i) . (take 3 . drop j)) xs | i <- [0, 3, 6], j <- [0, 3, 6]]
+boxes xs = map concat [(map (take 3 . drop i) . (take 3 . drop j)) xs | i <- [0, 3, 6], j <- [0, 3, 6]]
 
-blank :: Grod
-blink = replicate 9 (replicate 9 '.')
+blank :: Grid
+blank = replicate 9 (replicate 9 '.')
+
+hasDuplicates :: Eq a => [a] -> Bool
+hasDuplicates xs = not (length xs == length (nub xs))
+
+allFalse :: [Bool] -> Bool
+allFalse = all (== False)
+
+valid :: Grid -> Bool
+valid xs = (allFalse (map hasDuplicates (rows xs))) && (allFalse (map hasDuplicates (cols xs))) && (allFalse (map hasDuplicates (boxes xs)))
 
 main = do
   putStrLn "Hello, everybody!"
